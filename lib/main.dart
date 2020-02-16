b19d2909c33508a492490057782d743073a314ce
zhangyanling77 <18783226594@163.com>
2020-02-16 20:40:43 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
-
-// 引入 UI组件库
 import 'package:flutter/material.dart';
-// 引入 MyHomePage
 import 'home/MyHomePage.dart';
 
-// 定义入口函数
 void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // 我们的项目都需要写在 MaterialApp 的里面
+    // 项目的内容就需要写在 MaterialApp 的里面
     return MaterialApp(
       title: 'app',
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
       debugShowCheckedModeBanner: false,
-      // 传入我们的自定义的 组件
+      // 传入自定义的组件
       home: MyHomePage(),
     );
   }
